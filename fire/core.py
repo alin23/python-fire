970502f30bd0c9a832d793bfd42370919755dd0c
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-04-30 15:52:35 +0300

    Add support for coroutines

--- fire/core.py
+++ fire/core.py
@@ -49,9 +49,7 @@ The available flags for all Fire CLIs are:
   --trace: Get the Fire Trace for the command.
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
 import inspect
 import json
@@ -62,16 +60,19 @@ import shlex
 import sys
 import types
 
-from fire import completion
-from fire import decorators
-from fire import formatting
-from fire import helptext
-from fire import inspectutils
-from fire import interact
-from fire import parser
-from fire import trace
-from fire import value_types
+from fire import (
+    completion,
+    decorators,
+    formatting,
+    helptext,
+    inspectutils,
+    interact,
+    parser,
+    trace,
+    value_types,
+)
 from fire.console import console_io
+
 import six
 
 
@@ -669,7 +670,15 @@ def _CallAndUpdateTrace(component, args, component_trace, treatment='class',
   fn = component.__call__ if treatment == 'callable' else component
   parse = _MakeParseFn(fn, metadata)
   (varargs, kwargs), consumed_args, remaining_args, capacity = parse(args)
-  component = fn(*varargs, **kwargs)
+  if six.PY34:
+    import asyncio
+    if asyncio.iscoroutinefunction(fn):
+      loop = asyncio.get_event_loop()
+      component = loop.run_until_complete(fn(*varargs, **kwargs))
+    else:
+      component = fn(*varargs, **kwargs)
+  else:
+    component = fn(*varargs, **kwargs)
 
   if treatment == 'class':
     action = trace.INSTANTIATED_CLASS
